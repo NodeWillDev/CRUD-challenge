88fda4ccbe3ba7737cc8f9f3e99b80cb84900e86
NodeWillDev <williamdasilva.dev@gmail.com>
2025-03-19 15:29:02 -0300

    109° commit from 2025

---
+++ front-end/scripts/grids-style.js
@@ -0,0 +1,14 @@
+/** @type {HTMLDivElement} */
+const element = document.querySelector(".grid-style");
+
+const lines = 48;
+for (let i = 0; i < lines; i++) {
+  const children = document.createElement("div");
+  children.className = "grid-child";
+  children.style.setProperty(
+    "--delay",
+    `${Math.random() * (3 - 0.03) + 0.03}s`
+  );
+  children.appendChild(document.createElement("span"));
+  element.appendChild(children);
+}
